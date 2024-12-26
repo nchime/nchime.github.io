e6b7220e28799396856501ce5988672947337ca0
chkwak <chkwak@emart.com>
2024-12-27 07:23:26 +0900

    fix: build script

--- next.config.js
+++ next.config.js
@@ -54,8 +54,7 @@ const securityHeaders = [
   },
 ]
 
-// const output = process.env.EXPORT ? 'export' : undefined
-const output = 'export'
+const output = process.env.EXPORT ? 'export' : undefined
 const basePath = process.env.BASE_PATH || undefined
 const unoptimized = process.env.UNOPTIMIZED ? true : undefined
 
