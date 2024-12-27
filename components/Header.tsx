820ff5e60dc595353d3d1101bf7bf25b6d70b697
chkwak <chkwak@emart.com>
2024-12-27 13:48:00 +0900

    feat: 로고 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import 'animate.css';
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -16,8 +17,12 @@ const Header = () => {
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
-          <div className="mr-3">
-            <Logo />
+
+        
+        
+          <div className="mr-3 animate__animated animate__heartBeat">
+            {/* <Logo /> */}
+            <img src="/static/images/cartoon-satellite.png" width="50" />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
