471614dd840f7e13ba75772ce451f1c98796fe8f
chkwak <chkwak@emart.com>
2024-12-30 14:27:58 +0900

    feat: restore save

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,6 @@ import SearchButton from './SearchButton'
 import 'animate.css'
 import Image from 'next/image'
 
-
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
   if (siteMetadata.stickyNav) {
