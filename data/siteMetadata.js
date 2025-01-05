240bfd310701dd361e15d21b6b28673589e95d64
chkwak <nchime@gmail.com>
2025-01-05 11:26:56 +0900

    fix: 헤더 수정

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -13,7 +13,8 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  // siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/nchime_avatar.png`,  
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'nchime@gmail.com',
