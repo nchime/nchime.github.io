a347609b03198a4fb367e6f0c8c7323ae13e19f7
chkwak <nchime@gmail.com>
2025-01-07 00:06:54 +0900

    feat: 구글 노트북LM 소개

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -6,7 +6,8 @@ import blog__20250101DiscusMdx from './blog__20250101-discus.mdx.json' with { ty
 import blog__20250101GithubpageMdx from './blog__20250101-githubpage.mdx.json' with { type: 'json' }
 import blog__20250101SampleMdx from './blog__20250101-sample.mdx.json' with { type: 'json' }
 import blog__20250101StaticblogMdx from './blog__20250101-staticblog.mdx.json' with { type: 'json' }
+import blog__20250106GooglenotebooklmMdx from './blog__20250106-googlenotebooklm.mdx.json' with { type: 'json' }
 import blog__guideToUsingImagesInNextjsMdx from './blog__guide-to-using-images-in-nextjs.mdx.json' with { type: 'json' }
 import blog__introducingTailwindNextjsStarterBlogMdx from './blog__introducing-tailwind-nextjs-starter-blog.mdx.json' with { type: 'json' }
 
-export const allBlogs = [blog__20241226GithubactionMdx, blog__20241227NextjsblogMdx, blog__20250101DiscusMdx, blog__20250101GithubpageMdx, blog__20250101SampleMdx, blog__20250101StaticblogMdx, blog__guideToUsingImagesInNextjsMdx, blog__introducingTailwindNextjsStarterBlogMdx]
+export const allBlogs = [blog__20241226GithubactionMdx, blog__20241227NextjsblogMdx, blog__20250101DiscusMdx, blog__20250101GithubpageMdx, blog__20250101SampleMdx, blog__20250101StaticblogMdx, blog__20250106GooglenotebooklmMdx, blog__guideToUsingImagesInNextjsMdx, blog__introducingTailwindNextjsStarterBlogMdx]
