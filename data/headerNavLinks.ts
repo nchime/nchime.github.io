cff5eb9259d83171b944740d5051bcb49d005e4f
chkwak <chkwak@emart.com>
2025-01-08 20:59:08 +0900

    feat: 헤더 메뉴 테스트

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: 'https://satellite-app.vercel.app', title: 'Satellite' },
   { href: '/about', title: 'About' },
 ]
 
