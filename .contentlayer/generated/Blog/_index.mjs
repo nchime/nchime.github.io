e78ce726045adb55da4842c9a47c056428a635af
chkwak <chkwak@emart.com>
2025-01-24 15:24:08 +0900

    feat: 블로그 글 수정

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -7,8 +7,8 @@ import blog__20250101GithubpageMdx from './blog__20250101-githubpage.mdx.json' w
 import blog__20250106GooglenotebooklmMdx from './blog__20250106-googlenotebooklm.mdx.json' with { type: 'json' }
 import blog__20250107OpengraphMdx from './blog__20250107-opengraph.mdx.json' with { type: 'json' }
 import blog__20250116K9sMdx from './blog__20250116-k9s.mdx.json' with { type: 'json' }
+import blog__20250124MyblogMdx from './blog__20250124-myblog.mdx.json' with { type: 'json' }
 import blog__introducingTailwindNextjsStarterBlogMdx from './blog__introducing-tailwind-nextjs-starter-blog.mdx.json' with { type: 'json' }
 import blog__newfeatureMdx from './blog__newfeature.mdx.json' with { type: 'json' }
-import blog__20250124MyblogMdx from './blog__20250124-myblog.mdx.json' with { type: 'json' }
 
-export const allBlogs = [blog__20241226GithubactionMdx, blog__20241227NextjsblogMdx, blog__20250101DiscusMdx, blog__20250101GithubpageMdx, blog__20250106GooglenotebooklmMdx, blog__20250107OpengraphMdx, blog__20250116K9sMdx, blog__introducingTailwindNextjsStarterBlogMdx, blog__newfeatureMdx, blog__20250124MyblogMdx]
+export const allBlogs = [blog__20241226GithubactionMdx, blog__20241227NextjsblogMdx, blog__20250101DiscusMdx, blog__20250101GithubpageMdx, blog__20250106GooglenotebooklmMdx, blog__20250107OpengraphMdx, blog__20250116K9sMdx, blog__20250124MyblogMdx, blog__introducingTailwindNextjsStarterBlogMdx, blog__newfeatureMdx]
